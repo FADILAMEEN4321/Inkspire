8ebe7562be8ac4fb740ad19a2a1f60d0f9b43193
FADIL AMEEN V.M <129923924+FADILAMEEN4321@users.noreply.github.com>
2023-09-09 20:23:39 +0530

    put # in href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
   <nav className="navbar navbar-expand-lg" style={{ background: "gold" }}>
   <div className="container-fluid">
-  <a className="navbar-brand" href="/">
+  <a className="navbar-brand" href="#">
         </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
