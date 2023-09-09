6c9b8f0efeb4545aab557adec78aea2e5c5126b3
FADIL AMEEN V.M <129923924+FADILAMEEN4321@users.noreply.github.com>
2023-09-09 21:15:59 +0530

    updated the nav bar UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,15 @@ export default function Navbar() {
   <nav className="navbar navbar-expand-lg" style={{ background: "gold" }}>
   <div className="container-fluid">
   <a className="navbar-brand" href="#">
-        </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <h1 style={{ fontFamily: 'Lato', fontSize: '35px' }}>
+  <h1 style={{ fontFamily: 'Lato', fontSize: '35px' }}>
     Inkspire.
     </h1>
+        </a>
+    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button> */}
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    
     </div>
   </div>
 </nav>  
